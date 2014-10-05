c00f5b10ba1416fb367732d44a22002ea47a6808
sghose <ronnie.ghose@gmail.com>
2014-10-04 20:03:45 -0400

    merp

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-      res.render('index', { title: 'Hey', message: 'Hello there! ;)'});
+      response.render('index', { title: 'Hey', message: 'Hello there! ;)'});
 })
 
 
